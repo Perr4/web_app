5608ef52ffa9347a86317ade39c08a9cb4be7061
perr4 <giacomoperra16@gmail.com>
2025-09-28 16:38:16 +0200

    n°2

--- src/main/java/com/perra/web_app/HomeController.java
+++ src/main/java/com/perra/web_app/HomeController.java
@@ -2,19 +2,25 @@ package com.perra.web_app;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-
-
-
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
 
-
     @RequestMapping("/")
-    public String home (){
-        
+    public String home() {
+
         return "index.jsp";
+    }
+
+    @RequestMapping("add")
+    public String add(@RequestParam("num1")int nino,int num2,HttpSession session){
+
+    int result = nino +num2;
+        session.setAttribute("result", result);
+        return "result.jsp";
     }
 
 }
